7ea2030ec16f5ce1a1b5bbc8b865147db9de41a5
Andre Muniz <engenheiro.muniz@outlook.com>
2025-09-13 23:23:28 -0300

    The OrderStatus - enum class was created and other adjustments were made for the correct functioning of this class.

--- src/main/java/com/muniz/Spring/course/config/TestConfig.java
+++ src/main/java/com/muniz/Spring/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.muniz.Spring.course.entities.Order;
 import com.muniz.Spring.course.entities.User;
+import com.muniz.Spring.course.entities.enuns.OrderStatus;
 
 import repository.OrderRepository;
 import repository.UserRepository;
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner{
 		  User u1 = new User(null, "Mary Brown", "marybrown@gmail.com", "741 98546 985412", "487 548 9541");
 		  User u2 = new User(null, "Alex Green", "alexg@gmail.com", "574 45632 254789", "554 234 4564");		
 		  
-		  Order o1 = new Order(null,Instant.parse("2025-09-26T10:40:25Z"),u1);
-		  Order o2 = new Order(null,Instant.parse("2025-04-26T12:55:50Z"),u2);
-		  Order o3 = new Order(null,Instant.parse("2025-11-17T19:50:45Z"),u1);
+		  Order o1 = new Order(null,Instant.parse("2025-09-26T10:40:25Z"), OrderStatus.PAID, u1);
+		  Order o2 = new Order(null,Instant.parse("2025-04-26T12:55:50Z"), OrderStatus.WAITING_PAYMENT,u2);
+		  Order o3 = new Order(null,Instant.parse("2025-11-17T19:50:45Z"), OrderStatus.WAITING_PAYMENT,u1);
 		  
 		  userRepository.saveAll(Arrays.asList(u1,u2));		
 		  orderRepository.saveAll(Arrays.asList(o1,o2,o3));
